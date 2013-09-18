f2a12eaf64006b3180815edb16339a7e2f78a3f4
Ryan Linton <ryanlntn@gmail.com>
2013-09-18 11:28:35 -0700

    change left to right

--- lib/pro_motion_slide_menu/slide_menu_screen.rb
+++ lib/pro_motion_slide_menu/slide_menu_screen.rb
@@ -7,13 +7,13 @@ module ProMotionSlideMenu
     # SlideMenuScreen
     #
     # This is added as the root view controller when using the `open_slide_menu` method in your application delegate.
-    # 
-    # Several properties are defined to get the underlying PKRevealController instance for additional configuration, the 
+    #
+    # Several properties are defined to get the underlying PKRevealController instance for additional configuration, the
     # screen shown as the hidden menu, and the screen shown as the content controller.
     #
 
     def self.new(menu, content, options={})
-      screen = self.revealControllerWithFrontViewController(nil, leftViewController: nil, options: nil)
+      screen = self.revealControllerWithFrontViewController(nil, rightViewController: nil, options: nil)
       screen.on_create(options) if screen.respond_to?(:on_create)
       screen.menu_controller = menu unless menu.nil?
       screen.content_controller = content unless content.nil?
@@ -31,11 +31,11 @@ module ProMotionSlideMenu
     def menu_controller=(c)
       controller = prepare_controller_for_pm(c)
       controller = controller.navigationController || controller
-      self.setLeftViewController controller, focusAfterChange: true, completion: default_completion_block
+      self.setRightViewController controller, focusAfterChange: true, completion: default_completion_block
     end
 
     def menu_controller
-      self.leftViewController
+      self.rightViewController
     end
 
     def content_controller=(c)
